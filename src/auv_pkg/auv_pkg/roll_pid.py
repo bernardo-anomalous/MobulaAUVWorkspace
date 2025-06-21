e001a313e09848cdc2d29b166dfc83029bfaca49
bernardo-anomalous <bernardo@anomalous.company>
2025-06-21 20:39:37 +0700

    Reset PID states on activation

--- src/auv_pkg/auv_pkg/roll_pid.py
+++ src/auv_pkg/auv_pkg/roll_pid.py
@@ -83,6 +83,9 @@ class WingRollController(Node):
         if self.pid_active and not was_active:
             # Reset recovery ramp when PID control is re-enabled
             self.recovery_factor = 0.0
+            self.integral_error_roll = 0.0
+            self.previous_error_roll = 0.0
+            self.previous_correction_roll = 0.0
 
     def update_pid(self):
         if not self.pid_active:
